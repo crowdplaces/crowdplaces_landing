b46a90763aaab128d6c835e37e013fa65fd8f2ac
Henrique Bastos <henrique@bastos.net>
2012-05-01 18:30:47 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrowdplacesLanding::Application.config.secret_token = '0bc6acff668bb4a1f541df88ac1c44e4626d2936620b401eaf8688635f6e9ea2f25a21a58d5d6466754ce0397f0e53b9094d6e5451270e1f4b9a6f7e913fb66a'
