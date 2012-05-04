2d795f5fc2a7aa5c383f1e98abb4e53a0b60dc51
Henrique Bastos <henrique@bastos.net>
2012-05-04 19:06:51 -0400

    Add simple ACCEPT_LANGUAGE detection.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,13 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = detect_locale || I18n.default_locale
+  end
+
+  def detect_locale
+    # if explicit informed, take it. Otherwise try to detect browser's settings.
+    locale = params[:locale] || request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    I18n.available_locales.include?(locale.to_sym) ? locale : nil
   end
 
   protected
